aa5a986f38917db8311431ba4d1e9bd9fd018e0f
Sean Davis <sean@bluesabre.org>
2022-04-20 03:20:12 -0400

    Update daily-live manifest automatically

---
+++ .github/update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+RELEASE="jammy"
+ARCHES="amd64"
+DATE=`date -u +%Y%m%d`
+URL=""
+
+for ARCH in `echo $ARCHES`; do
+    URL="http://cdimage.ubuntu.com/xubuntu/daily-live/current/$RELEASE-desktop-$ARCH.manifest"
+    if wget -q --method=HEAD "$URL"; then
+        echo $URL
+        wget -O "desktop-$ARCH.manifest" "$URL"
+    fi
+done
