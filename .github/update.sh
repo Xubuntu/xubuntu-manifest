67afd45e9de0ed5083deea3d400912390ba7fc25
Sean Davis <sean@bluesabre.org>
2023-02-15 05:57:47 -0500

    Add minimal manifest to daily-live

--- .github/update.sh
+++ .github/update.sh
@@ -2,13 +2,16 @@
 
 RELEASE="lunar"
 ARCHES="amd64"
+PROJECTS="desktop minimal"
 DATE=`date -u +%Y%m%d`
 URL=""
 
 for ARCH in `echo $ARCHES`; do
-    URL="http://cdimage.ubuntu.com/xubuntu/daily-live/current/$RELEASE-desktop-$ARCH.manifest"
-    if wget -q --method=HEAD "$URL"; then
-        echo $URL
-        wget -O "desktop-$ARCH.manifest" "$URL"
-    fi
+    for PROJECT in `echo $PROJECTS`; do
+        URL="http://cdimage.ubuntu.com/xubuntu/daily-live/current/$RELEASE-$PROJECT-$ARCH.manifest"
+        if wget -q --method=HEAD "$URL"; then
+            echo $URL
+            wget -O "$PROJECT-$ARCH.manifest" "$URL"
+        fi
+    done
 done
